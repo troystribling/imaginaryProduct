e06428f3f04aed4e4572f21c68e73966d59f03f9
Troy Stribling <troy.stribling@gmail.com>
2013-08-07 23:10:33 -0700

    more drumming

--- research/beats.rb
+++ research/beats.rb
@@ -1,5 +1,5 @@
 set_config(:beats_per_minute=>120, :time_signature=>'4/4', :resolution=>'1/64')
 
-str 'on_beat_kick', [pr(:bass_drum_1), pr(:electric_snare), pr(:bass_drum_1), n(:R)], :ch=> 0 do |pattern|
+str 'on_beat_kick', [pr(:bass_drum_1), pr(:crash_cymbal_1), pr(:bass_drum_1), pr(:splash_cymbal)], :ch=> 0 do |pattern|
   pattern
 end
