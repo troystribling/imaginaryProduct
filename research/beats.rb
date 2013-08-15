286a23b0b3d85e68bd6cd029d07a5a6c982f4edd
Troy Stribling <troy.stribling@gmail.com>
2013-08-14 23:24:23 -0700

    added motorik and a few drum kits that need refactoring

--- research/beats.rb
+++ research/beats.rb
@@ -1,7 +1,9 @@
 ## Drum Kit
+#
 ## :bass_drum_1
 ## :side_stick
 ## :acoustic_snare
+## :hand_clap
 #
 ## :electric_snare
 ## :low_floor_tom
@@ -22,22 +24,38 @@
 set_config(:beats_per_minute=>120, :time_signature=>'4/4', :resolution=>'1/64')
 
 
-str 'kicking-1-2', [pr(:bass_drum_1), n(:R), pr(:bass_drum_1), n(:R)], :ch=> 0 do |pattern|
+str 'kicking-1-3', [pr(:bass_drum_1), n(:R), pr(:bass_drum_1), n(:R)], :ch=>0 do |pattern|
   pattern
 end
 
-str 'kicking-and-crashing', nl(pr(:bass_drum_1, :l => 8), pr(:crash_cymbal_2, :l => 8)), :ch=> 0 do |pattern|
+str 'kicking-and-crashing', nl(pr(:bass_drum_1, :l => 8), pr(:crash_cymbal_2, :l => 8)), :ch=>0 do |pattern|
   pattern
 end
 
-str 'toms', [pr(:low_floor_tom, :l => 16), pr(:low_tom, :l => 16), n(:R, :l=>16) , pr(:low_mid_tom, :l => 16)], :ch=> 0 do |pattern|
+str 'motorik', nil, :ch=>0 do |pattern|
+  len = 8
+  [nl(pr(:bass_drum_1,:l=>len),pr(:open_hi_hat, :l=>len)),
+   nl(pr(:bass_drum_1,:l=>len),pr(:open_hi_hat, :l=>len)),
+   nl(pr(:acoustic_snare,:l=>len),pr(:open_hi_hat, :l=>len)),
+   nl(pr(:bass_drum_1,:l=>len),pr(:open_hi_hat, :l=>len)),
+   nl(pr(:bass_drum_1,:l=>len),pr(:open_hi_hat, :l=>len)),
+   nl(pr(:bass_drum_1,:l=>len),pr(:open_hi_hat, :l=>len)),
+   nl(pr(:acoustic_snare,:l=>len),pr(:open_hi_hat, :l=>len)),
+   nl(pr(:bass_drum_1,:l=>len),pr(:open_hi_hat, :l=>len))]
+end
+
+fast_speed = 32
+str 'gallop', [pr(:low_floor_tom, :l => fast_speed),
+               pr(:low_tom, :l => fast_speed),
+               n(:R, :l=> fast_speed),
+               pr(:high_mid_tom, :l => fast_speed)], :ch=>0 do |pattern|
   pattern
 end
 
 if sources.include?('nanoKONTROL')
+  fast_speed = 32
   add_input('nanoKONTROL')
   add_cc(:switch, 13, :type => :switch)
-  fast_speed = 32
   str 'your-beaches', nil, :ch=>0 do |pattern|
     if cc(:switch)
       [pr(:bass_drum_1, :l => fast_speed/2), pr(:low_mid_tom, :l => fast_speed/2)]
